244677c420a63d5617ad9e05d23c1d5589f747f2
Isi <ishak.zehhaf@gmail.com>
2020-08-25 16:55:37 +0200

    implementazione Add_Esame e le due attività Limiti-Zehhaf

--- app/src/main/java/pdm/unindubria/examcountdown/MainActivity.java
+++ app/src/main/java/pdm/unindubria/examcountdown/MainActivity.java
@@ -1,14 +1,36 @@
 package pdm.unindubria.examcountdown;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton floatingActionButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddExam.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
